0773892400a572ff13da1b2c35887bd467e3c6c6
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2024-05-23 08:32:33 +0800

    create new project

--- src/utility/supabase-client.ts
+++ src/utility/supabase-client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://usfyipcplxyqrrbeaiwl.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzZnlpcGNwbHh5cXJyYmVhaXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNDEwOTIsImV4cCI6MjAzMTkxNzA5Mn0.1EiujZPvcU7whQORhqngBuBu63s-QT2_qBSndtGtA2s";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
